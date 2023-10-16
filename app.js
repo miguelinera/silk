3e88ed0e5f5c1dacc3abe2a1068e0e02012329e8
Miguel Linera Alperi <miguelinera@MacBook-Pro-de-Miguel.local>
2023-10-16 09:57:19 +0200

    Second commit

--- app.js
+++ app.js
@@ -9,3 +9,15 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/mydatabase', {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Could not connect to MongoDB', err));
+
+app.post('/register', (req, res) => {
+  // Registration logic here
+});
+
+const { authenticate } = require('../middleware/auth');
